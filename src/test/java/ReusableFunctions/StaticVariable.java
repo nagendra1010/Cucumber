65cf761bdfd91f8565650e2ff1c91cfc8e01fd61
nagendra1010@gmail.com <nagendra1010@gmail.com>
2020-09-21 16:39:46 +0530

    second commit

--- src/test/java/ReusableFunctions/StaticVariable.java
+++ src/test/java/ReusableFunctions/StaticVariable.java
@@ -4,7 +4,7 @@ import cucumber.api.Scenario;
 public class StaticVariable {
 	public static String MAX_WAIT_TIME="10";
 	public static String PATH="\\target\\cucumber-reports\\Screenshot\\";
-	public static String screenshot="false";
+	public static String screenshot="true";
 	public static String Application="Shopping portal";
 	public static Scenario message=null;
 }
